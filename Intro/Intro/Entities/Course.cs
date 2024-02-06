ecb24bd67f6d014718f052fe41bf7f8fbb006867
sulenursekmen <sulenursekmenn@gmail.com>
2024-02-06 20:16:15 +0300

    added basic method and classwork

---
+++ Intro/Intro/Entities/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro.Entities;
+
+public class Course
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    // prop ile hızlı oluştur
+    public double Price { get; set; }
+}
---
+++ Intro/Intro/Program.cs
@@ -0,0 +1,41 @@
+﻿// Program.cs: Ana program dosyası
+
+using Intro.Entities;
+using System;
+
+// Main method
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Hoşgeldin mesajı
+        string welcomeMessage = "C# 'a yeniden merhaba";
+        Console.WriteLine(welcomeMessage);
+
+        // Kurs nesnelerinin oluşturulması ve değerlerinin atanması
+        Course cSharpCourse = new Course() { Id = 1, Name = "C# Kursu", Description = ".NET 6 ile Modern C# Programlama", Price = 0 };
+        Course flutterCourse = new Course() { Id = 2, Name = "Flutter Kursu", Description = "Mobil Uygulama Geliştirme", Price = 0 };
+        Course htmlCourse = new Course() { Id = 3, Name = "HTML Kursu", Description = "Web Geliştirme Temelleri", Price = 0 };
+
+        // Kursların bir dizi içine alınması
+        Course[] courses = { cSharpCourse, flutterCourse, htmlCourse };
+
+        // Kursların listelenmesi
+        Console.WriteLine("----- Tüm Kurslar -----");
+        foreach (Course course in courses)
+        {
+            Console.WriteLine(course.Name + " / " + course.Description);
+        }
+
+        // Kurs yöneticisinin oluşturulması
+        CourseManager courseManager = new CourseManager();
+
+        // Kursların yeniden alınması ve listelenmesi
+        Course[] coursesFromManager = courseManager.GetAll();
+        Console.WriteLine("\n----- Tüm Kurslar (Yöneticiden) -----");
+        foreach (Course course in coursesFromManager)
+        {
+            Console.WriteLine(course.Name + " / " + course.Description);
+        }
+    }
+}
