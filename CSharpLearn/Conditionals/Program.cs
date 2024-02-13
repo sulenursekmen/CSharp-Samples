b8eb69b20ae13f638ef3ad35d5e74e813321c46d
sulenursekmen <sulenursekmenn@gmail.com>
2024-02-14 00:36:56 +0300

    Added Conditionals and TypesAndVariables

---
+++ CSharpLearn/Conditionals/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var number1 = 20;
+
+            // Using if-else statements to check the value of number1
+            if (number1 == 10)
+            {
+                Console.WriteLine("Number is 10");
+            }
+            else if (number1 == 20)
+            {
+                Console.WriteLine("Number is 20");
+            }
+            else
+            {
+                Console.WriteLine("Number is not 10 || 20");
+            }
+
+            // Using ternary conditional operator to achieve the same
+            Console.WriteLine(number1 == 10 ? "Number is 10" : number1 == 20 ? "Number is 20" : "Number is not 10 || 20");
+
+            // Switch case to determine the day corresponding to the entered number
+            Console.WriteLine("Please enter a day number (1-7):");
+            int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+            string dayName = "";
+
+            switch (dayNumber)
+            {
+                case 1: dayName = "Monday"; break;
+                case 2: dayName = "Tuesday"; break;
+                case 3: dayName = "Wednesday"; break;
+                case 4: dayName = "Thursday"; break;
+                case 5: dayName = "Friday"; break;
+                case 6: dayName = "Saturday"; break;
+                case 7: dayName = "Sunday"; break;
+                default: dayName = "Invalid day number"; break;
+            }
+
+            // Using if-else statements to check the range of number1
+            if (number1 >= 0 && number1 <= 100)
+            {
+                Console.WriteLine("The number is between 0 and 100.");
+            }
+            else if (number1 < 0)
+            {
+                Console.WriteLine("The number is less than 0.");
+            }
+            else
+            {
+                Console.WriteLine("The number is greater than 100.");
+            }
+
+            // Printing the day corresponding to the entered number
+            Console.WriteLine($"The day corresponding to the entered number: {dayName}");
+            int number = 15;
+
+            // Check if the number is non-negative
+            if (number >= 0)
+            {
+                Console.WriteLine("Number is non-negative.");
+
+                // If the number is non-negative, check if it is even or odd
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine("Number is even.");
+                }
+                else
+                {
+                    Console.WriteLine("Number is odd.");
+                }
+            }
+            else
+            {
+                // If the number is negative, print a message
+                Console.WriteLine("Number is negative.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpLearn/TypesAnsVariables/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndVariables
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Value Types
+            // Integer Data Types   
+            // The 'int' type's range is between -2,147,483,648 and 2,147,483,647 (-2^31 to +2^31-1) / 32 bits  
+            int number1 = 10;
+            int number2 = 20;
+            // The 'long' type's range is between -2^63 (-9,223,372,036,854,775,808) and +2^63-1 (9,223,372,036,854,775,807) / 64 bits 
+            long number3 = 30;
+            // The 'short' type's range is between -2^15  (-32,768) and +2^15-1 (32,767) / 16 bits  
+            short number4 = 35;
+            // The 'byte' type's range is between -128 and +127 / 8 bits
+            byte number5 = 6;
+
+            // {0}{1} indicates index
+            Console.WriteLine("Number1 is {0}{1}{2}{3}{4}", number1, number2, number3, number4, number5);
+
+            // Boolean Data Type
+            // Occupies 1 bit of memory, can hold either 'true' or 'false'
+            bool isLoading = true;
+
+            // Char Data Type
+            // Written with single quotes, occupies 16 bits of memory  
+            char character = 'a';
+
+            Console.WriteLine(isLoading.ToString() + (int)character);
+
+
+            // Floating Point Numbers 
+            // The 'double' type holds 64 bits, providing 15-16 digits of precision
+            double number6 = 5.4;
+            // The 'decimal' type holds larger 28-digit numbers, appending 'M' at the end to indicate decimal
+            decimal number7 = 10.8M;
+            Console.WriteLine(number6.ToString() + " / " + number7.ToString());
+
+            //enum
+            Console.WriteLine((int)Days.Sunday);
+
+            // var 
+            var example = "Sule Nur";
+            Console.WriteLine(example);
+
+            // String Variable
+            // A string variable is essentially an array of characters.
+
+            // Console.WriteLine("Hello to C# Again");
+            // Waits for the user to perform an operation (press a key)
+            Console.ReadLine(); 
+        }
+
+        // Enum Data Type
+        enum Days
+        {
+            Monday=5, Tuesday, Wednesday, Friday, Sunday
+        }
+
+    }
+}
