ad25426bdd506cd2a69ca137be89e1ab219586fb
sulenursekmen <sulenursekmenn@gmail.com>
2024-02-15 18:18:41 +0300

    Added RecapDemo-Checkers Game Board

---
+++ CSharpLearn/Classes/Car.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Classes
+{
+    public class Car
+    {
+        //field
+        //public string Name;
+        // Properties
+
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        private string _firstName;
+
+        //Encapsulation
+        public string Name 
+        {
+            get 
+            {
+                return "New " + _firstName;
+            } 
+            set 
+            {
+                _firstName = value;
+            } 
+        }
+
+        
+        // Methods
+        public void StartEngine()
+        {
+            Console.WriteLine("Car engine started.");
+        }
+
+        public void StopEngine()
+        {
+            Console.WriteLine("Car engine stopped.");
+        }
+
+        public void Accelerate()
+        {
+            Console.WriteLine("Car is accelerating.");
+        }
+
+        public void Brake()
+        {
+            Console.WriteLine("Car is braking.");
+        }
+    }
+}
---
+++ CSharpLearn/Classes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            car.Model = "Toyota Camry";
+            car.Year = 2020;
+
+            Car car2 = new Car 
+            {
+               Model="Astra",
+               Year=2005,
+               Name= "Opel",
+            };
+
+            Console.WriteLine(car2.Model);
+            Console.WriteLine(car2.Name);
+
+
+            car.StartEngine();
+
+            car.Accelerate();
+
+            car.Brake();
+
+            car.StopEngine();
+
+            
+
+            Console.ReadLine();
+
+        }
+    }
+}
