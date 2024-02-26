e848152a0a3b8fd52f2fc80ddc5968514cf746de
sulenursekmen <sulenursekmenn@gmail.com>
2024-02-26 17:05:47 +0300

    Added Web Api Project

---
+++ DotnetWebApiProject/Business/Dtos/Requests/CreateBrandRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Dtos.Requests;
+
+public class CreateBrandRequest
+{
+    /// <summary>
+    /// //
+    /// </summary>
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedDate { get; set; }
+}
---
+++ DotnetWebApiProject/DataAccess/Abstracts/IBrandDal.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstracts;
+
+//DAL Data Access Layer
+public interface IBrandDal
+{
+    void Add(Brand brand);
+    List<Brand> GetAll();
+
+}
