b9f9e0f4672dcbc3ed72bc1272288344a11187b3
sulenursekmen <sulenursekmenn@gmail.com>
2024-02-26 23:59:56 +0300

    Editted TaskForm app and Added TaskWebApi

---
+++ AsenkronAndMultiThread/TaskWebApp.API/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskWebApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetContentAsync()
+        {
+            // 
+
+            var myTask = new HttpClient().GetStringAsync("https://www.google.com");
+
+            // other transactions
+
+            var data = await myTask;
+
+            return Ok(data);
+
+        }
+    }
+}
